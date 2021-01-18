1fc9915e2d310aa38440ec497268a02d73a1adc7
notaryzw3b <modz.ayrton@gmail.com>
2021-01-18 22:29:58 +0100

    removed all zindex for reformat

--- src/Shroom/RoomEngine.js
+++ src/Shroom/RoomEngine.js
@@ -13,10 +13,11 @@ import Emitter from '../Emitter'
 class RoomEngine {
     constructor() {
         var view = document.querySelector("#canva1");
-        var application = new PIXI.Application({ view });
+        var application = new PIXI.Application({ view, resizeTo: window });
         this.shroom = Shroom.create({ application, resourcePath: "http://127.0.0.1:8081/resources" })
         this.appl = application
         this.room = null;
+        this.avatar = null;
     }
     createRoom(tiles) {
         if(this.room != null){
@@ -26,17 +27,21 @@ class RoomEngine {
         this.room = Room.create(this.shroom, {
             tilemap: tiles
         })
+        this.room.onTileClick = (pos) => {
+            this.avatar.move(pos.roomX, pos.roomY, pos.roomZ)
+        }
+        this.addUser()
         this.appl.stage.addChild(RoomCamera.forScreen(this.room));
     }
     addUser(){
-        const avatar = new Avatar({
+        this.avatar = new Avatar({
             look: "hd-180-1.hr-100-61.ch-210-66.lg-280-110.sh-305-62",
             direction: 4,
-            roomX: 0,
-            roomY: 0,
+            roomX: 5,
+            roomY: 22,
             roomZ: 0,
           });
-          this.room.addRoomObject(avatar)
+          this.room.addRoomObject(this.avatar)
     }
 
 }
